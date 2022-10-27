1e233ae6018f166fd4baa32ccb465523805cd523
JohnMVan <john.vanhessche@gmail.com>
2022-10-27 17:34:12 -0500

    week 2 assignments

---
+++ week-2/vanhessche_myworld.py
@@ -0,0 +1,10 @@
+"""
+Title: vanhessche_myworld.py
+Author:  John Vanhessche
+Date:  26 October 2022
+Description:  First python file, per Exercise 2.3
+
+"""
+
+
+print("You are now in Vanhessche's world!")
